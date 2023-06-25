ae1f1f31e735a2c298db81050cb30f97eda0d1c3
teguhmoch <85119832+teguhmoch@users.noreply.github.com>
2023-06-25 16:56:16 +0700

    updated middleware

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -15,12 +15,6 @@ class UserMiddleware
         if ($session == null) {
             return redirect()->route('user.login')->with('error', 'Please login first.');
         }
-
-        if ($session->status = 'inactive') {
-            // $request->session()->flush();
-            return redirect()->route('user.login')->with('error', 'Your account status inactive, please contact admin to actived your account !');
-        }
-
         return $next($request);
     }
 }
